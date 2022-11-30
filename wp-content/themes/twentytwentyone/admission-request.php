d8ab5ff6b05fbcf7121fdbfbf4e7f45e5dc2d4c5
Ritesh Das <ritesh9093@gmail.com>
2022-11-30 20:54:51 +0530

    Admission Req Page add

---
+++ wp-content/themes/twentytwentyone/admission-request.php
@@ -0,0 +1,174 @@
+<?php /* Template Name: NBPS Admission Request Page */ ?>
+<?php get_header('nbps'); ?> 
+<style>
+    .banner-area {
+        background: rgba(0, 0, 0, 0) url(<?php the_post_thumbnail_url(); ?>) no-repeat scroll center center / cover !important;
+        position: relative;
+        width: 100%;
+        z-index: 1;
+    }
+</style>        
+        <div class="banner-area-wrapper">
+            <div class="banner-area text-center" >	
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="banner-content-wrapper">
+                                <div class="banner-content">
+                                <?php the_title( '<h2>', '</h2>' ); ?>
+                                <?php // twenty_twenty_one_post_thumbnail(); ?>
+                                </div> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+		<div class="container">
+            <div class="row">
+                <div class="sliderrowarea">
+                    <marquee attribute_name = "attribute_value"....more attributes> <p>Early Bird Admissions Started! Join World’s #1 Leading Online School with 7500+ happy students!</p>
+                    </marquee>
+                </div>
+            </div>
+       </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="appliaraemy newsletter-form mc_embed_signup">
+        		 <form action="" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        		 <div class="row">
+        		  <div class="col-lg-9">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter Application No." required>
+        		  </div>
+        		  <div class="col-lg-3">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter Roll No." required>
+        		  </div>
+        		  <div class="col-sm-12">
+        		  <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter Student Name" required>
+        		  </div>
+        		  <div class="col-lg-9">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter Father Name" required>
+        		  </div>
+        		  <div class="col-lg-3">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Qualification" required>
+        		  </div>
+        		  <div class="col-lg-9">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter Mother Name" required>
+        		  </div>
+        		  <div class="col-lg-3">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Qualification" required>
+        		  </div>
+        		  <div class="col-lg-9">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Date Of Birth" required>
+        		  </div>
+        		  <div class="col-lg-3">
+        		    <input type="file" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Birth Certificte" required>
+        		  </div>
+        		  <div class="col-sm-12">
+        		  <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter Address" required>
+        		  </div>
+        		  <div class="col-lg-6">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Telephone No." required>
+        		  </div>
+        		  <div class="col-lg-6">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Mobile No." required>
+        		  </div>
+        		  <div class="col-lg-6">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Name of School last attended" required>
+        		  </div>
+        		  <div class="col-lg-3">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Class to which admitted" required>
+        		  </div>
+        		  <div class="col-lg-3">
+        		    <input type="file" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Birth Certificte" required>
+        		  </div>
+        		  <div class="col-lg-2">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Last Exam Passed" required>
+        		  </div>
+        		  <div class="col-lg-3">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Name Of Bord" required>
+        		  </div>
+        		  <div class="col-lg-2">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Total Marks" required>
+        		  </div>
+        		  <div class="col-lg-2">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Division / Percentage" required>
+        		  </div>
+        		  <div class="col-lg-3">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Remarks" required>
+        		  </div>
+        		  <div class="col-lg-9">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Parent’s Occupation" required>
+        		  </div>
+        		  <div class="col-lg-3">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Annual Income" required>
+        		  </div>
+        		  <div class="col-lg-6">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Religion" required>
+        		  </div>
+        		  <div class="col-lg-6">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Caste" required>
+        		  </div>
+        		  <div class="col-lg-6">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Student Aadhar Card No." required>
+        		  </div>
+        		  <div class="col-lg-6">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Students Bank Account No." required>
+        		  </div>
+        		  
+        		  <div class="col-lg-12">
+        		    <h3 style="text-align:left; font-weight:600;">No. of Documents Attached</h3>
+        		  </div>
+        		  
+        		  <div class="col-lg-9">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Documents Name" required>
+        		  </div>
+        		  <div class="col-lg-3">
+        		    <input type="file" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Documents" required>
+        		  </div>
+        		  <div class="col-lg-9">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Documents Name" required>
+        		  </div>
+        		  <div class="col-lg-3">
+        		    <input type="file" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Documents" required>
+        		  </div>
+        		  <div class="col-lg-9">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Documents Name" required>
+        		  </div>
+        		  <div class="col-lg-3">
+        		    <input type="file" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Documents" required>
+        		  </div>
+        		  
+        		   <div class="col-lg-12">
+        		    <h3 style="text-align:left; font-weight:600;">For Office Use Only (Admit Card)</h3>
+        		  </div>	
+        		  
+        		  <div class="col-lg-4">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Name of Student" required>
+        		  </div>
+        		  <div class="col-lg-4">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Admission No" required>
+        		  </div>
+        		  <div class="col-lg-4">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Roll No." required>
+        		  </div>
+        		  
+        		   <div class="col-lg-4">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Class to which admitted" required>
+        		  </div>
+        		  <div class="col-lg-4">
+        		    <input type="text" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Date" required>
+        		  </div>
+        		 
+        		   <div class="col-lg-4">
+        		    <button id="" class="default-btn" type="submit" name="subscribe" style="background: #f58220; overflow:hidden; position:relative;"><span>Submit</span></button> 
+        		  </div>
+        		  </div>
+		  
+		  
+		  
+		  </form>
+		 </div>
+
+<?php get_footer('nbps'); ?>
